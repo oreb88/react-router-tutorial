2ff3c42962f2b36f0a6db1c669ae2b76b72773cb
oreb <javabt01@gmail.com>
2023-10-16 23:49:00 +0900

    first

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import {Link} from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>홈</h1>
+            <p>가장 먼저 보이는 페이지</p>
+            <ul>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+                <li>
+                    <Link to="/profiles/velopert">velopert 프로필</Link>
+                </li>
+                <li>
+                    <Link to="/profiles/gildong">gildong 프로필</Link>
+                </li>
+                <li>
+                    <Link to="/profiles/unknown">unknown 프로필</Link>
+                </li>
+
+            </ul>
+        </div>
+    );
+}
+
+export default Home;
